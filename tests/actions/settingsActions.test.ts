e6fcbc3c073c88c9d3308657a0eb094aa33211b1
Abdelhadi <naimi.abdelhadi.97@gmail.com>
2018-08-22 12:29:11 +0100

    settingsActions.test

---
+++ tests/actions/settingsActions.test.ts
@@ -0,0 +1,33 @@
+import * as actions from "../../src/actions/settingsActions";
+
+describe("actions", () => {
+  it("should create an action to toggle the sound on and off", () => {
+    const expectedAction = {
+      type: actions.TOGGLE_SOUND
+    };
+    expect(actions.toggleSound()).toEqual(expectedAction);
+  });
+
+  it("should create an action to toggle the notifications on and off", () => {
+    const expectedAction = {
+      type: actions.TOGGLE_NOTIFICATION
+    };
+    expect(actions.toggleNotification()).toEqual(expectedAction);
+  });
+
+  it("should create an action to change the refreshing rate delay", () => {
+    const expectedAction = {
+      delay: 5,
+      type: actions.CHANGE_DELAY
+    };
+    expect(actions.changeDelay(5)).toEqual(expectedAction);
+  });
+
+  it("should create an action to call the notification API", () => {
+    const expectedAction = {
+      message: "hello",
+      type: actions.CALL_NOTIFICATION
+    };
+    expect(actions.callNotification("hello")).toEqual(expectedAction);
+  });
+});
