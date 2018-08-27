4310116c1c27d97ff778e29ce3b2767a78c4cf96
Abdelhadi <naimi.abdelhadi.97@gmail.com>
2018-08-27 19:37:54 +0100

    Adding simulated events test

--- tests/components/ErrorMessage.test.tsx
+++ tests/components/ErrorMessage.test.tsx
@@ -9,10 +9,10 @@ describe("<ErrorMessage/>", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("should call mock function when the button is clicked", () => {
+  it("should call mock function once when the button is clicked", () => {
     const mockFn = jest.fn();
     const wrapper = shallow(<ErrorMessage message="hello" clearError={mockFn} />);
     wrapper.find("button").simulate("click");
-    expect(mockFn).toHaveBeenCalled();
+    expect(mockFn).toHaveBeenCalledTimes(1);
   });
 });
--- tests/components/RedditAdder.test.tsx
+++ tests/components/RedditAdder.test.tsx
@@ -1,24 +1,65 @@
-import { shallow } from "enzyme";
-import * as React from 'react'
+import { mount, shallow } from "enzyme";
+import * as React from "react";
 
-import RedditAdder,{ IProps } from "../../src/components/RedditAdder";
+import RedditAdder, { IProps } from "../../src/components/RedditAdder";
 
-
-const props : IProps = {
+const makeProps = (): IProps => ({
   clearError: jest.fn(),
-  error: "error",
+  error: "",
   handleChange: jest.fn(),
   handleOpenSettingsModal: jest.fn(),
   handleSubmit: jest.fn(),
-  value: "hello"
-};
+  value: ""
+});
 
 describe("<RedditAdder/>", () => {
   it("should render correctly", () => {
-    
-    const wrapper = shallow(<RedditAdder {...props } />);
-
-
+    const props = makeProps();
+    const wrapper = shallow(<RedditAdder {...props} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should have <ErrorMessage> if error is not empty string", () => {
+    const props = Object.assign(makeProps(), { error: "error" });
+    const wrapper = shallow(<RedditAdder {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should call handleOpenSettingsModal only once when the button is clicked", () => {
+    const props = makeProps();
+    const wrapper = shallow(<RedditAdder {...props} />);
+    wrapper
+      .find("button")
+      .at(1)
+      .simulate("click");
+
+    expect(props.handleOpenSettingsModal).toHaveBeenCalledTimes(1);
+  });
+
+  it("should call handleChange when the input is changed", () => {
+    const props = makeProps();
+    const wrapper = shallow(<RedditAdder {...props} />);
+    wrapper
+      .find("input")
+      .simulate("change", { target: { name: "", value: "hello" } });
+
+    expect(props.handleChange).toBeCalledWith({
+      target: { name: "", value: "hello" }
+    });
+  });
+
+  it("should call handleSubmit when the button is clicked or the form submitted", () => {
+    let props = makeProps();
+    let wrapper = mount(<RedditAdder {...props} />);
+    wrapper
+      .find("button")
+      .at(0)
+      .simulate("submit");
+    expect(props.handleSubmit).toHaveBeenCalledTimes(1);
+
+    props = makeProps();
+    wrapper = mount(<RedditAdder {...props} />);
+    wrapper.find("form").simulate("submit");
+    expect(props.handleSubmit).toHaveBeenCalledTimes(1);
   });
 });
